ee05afe0ff83c0be742b8972605d266437fd4227
thomaspujol69 <32071308+thomaspujol69@users.noreply.github.com>
2019-05-22 22:33:22 +0200

    Resolve bugs

---
+++ classe.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Annales - Institution des Chartreux</title>
+    </head>
+    <body>
+        <h1><a href="./">Annales - Institution des Chartreux</a></h1>
+            
+            
+            
+<?php
+$classe = $_POST["classe"];
+if(isset($classe)){
+   echo "Vous allez accéder aux fichiers en rapport avec la classe : ".$classe.'<br><br><br>';
+}
+
+// Affichage fichiers
+
+$matiere = $_GET['var'];
+$dossier = 'files';
+$iterator = new DirectoryIterator($dossier);
+
+foreach($iterator as $fichier){
+   // La fonction isDot retourne TRUE si l'élement courant est "." ou ".."
+  if(!$fichier->isDot()){
+   // Affiche le fichier avec le lien
+      $file = $fichier->getFilename();
+      // On ne garde que le nom de la matière
+      $name = substr($file, 0, strlen($file)-12 );
+      // echo $name.'<br><p>OK2</p><br>';
+      $nom = $matiere.'_'.$classe;
+      // echo $matiere.'_'.$classe.'<p>OK3</p><br>';
+      if($name == $nom){
+        echo '<a href="./files/'.$file.'">'.$file.'</a> <br><br>';
+      }
+    }
+}
+?>
+
+    </body>
+</html>
--- matiere.php
+++ matiere.php
@@ -1,6 +1,59 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Annales - Institution des Chartreux</title>
+    </head>
+    <body>
+        <h1><a href="./">Annales - Institution des Chartreux</a></h1>
+
+
 <?php
-if(isset($_POST["matiere"])){
-   echo "Vous allez accéder aux fichiers en rapport avec la matière : ".$_POST["matiere"];
-   
+$matiere = $_POST["matiere"];
+if(isset($matiere)){
+   echo "Vous allez accéder aux fichiers en rapport avec la matière : ".$matiere.'<br><br>';
+           
+}
+
+// Préparation et affichage liste déroulante
+
+echo '<p>Choisissez la classe : </p>';
+
+// if not (isset($_POST["matiere"])){
+echo '<form name="classe" method="post" action="classe.php?var='.$matiere.'">';
+$classes = ['Trle','1ère','2nde','3ème','4ème','5ème','6ème'];
+$liste = '<select id="classe" name="classe">';
+// echo '<form id="classe" method="POST" action="classe.php?var='.$matiere.'"> ';
+
+// Pour chaque matière :
+foreach($classes as $classe){
+    $liste .= '<option value="'.$classe.'">'.$classe.'</option>';
+}
+$liste .= '</select>';
+echo $liste;
+echo '<br><br><input name="OK" type="submit" value="Valider"><br><br><br>';
+// }
+echo '</form>';
+
+// Affichage fichiers
+$dossier = 'files';
+$iterator = new DirectoryIterator($dossier);
+// $files = array();
+foreach($iterator as $fichier){
+   // La fonction isDot retourne TRUE si l'élement courant est "." ou ".."
+  if(!$fichier->isDot()){
+   // Affiche le fichier avec le lien
+      $file = $fichier->getFilename();
+      // On ne garde que le nom de la matière
+      //echo $file.'<br>';
+      $name = substr($file, 0, strlen($file)-18 );
+      //echo $name.'<br>';
+      if($name==$matiere){
+        echo '<a href="./files/'.$file.'">'.$file.'</a> <br><br>';
+      }
+    }
 }
 ?>
+        
+    </body>
+</html>
